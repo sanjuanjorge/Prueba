4686c6a71d1304a2f909f175440e83888c60d65b
sanjuanjorge <sanjuan@comersan.com>
2016-11-10 12:12:30 +0100

    creo el repositirio

--- prueba/ViewController.swift
+++ prueba/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var n = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
